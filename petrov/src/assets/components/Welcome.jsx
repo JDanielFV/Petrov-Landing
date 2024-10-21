342ff83668bf81d0961171c174f2d5e7228d17e3
J. Daniel Flores Vega <119628732+JDanielFV@users.noreply.github.com>
2024-10-21 12:00:12 -0600

    Pantalla de bienvenida terminada y responsiva

--- petrov/src/assets/components/Welcome.jsx
+++ petrov/src/assets/components/Welcome.jsx
@@ -1,37 +1,74 @@
 import styled from 'styled-components'
 
 const WelcomeContainer = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   height: 100%;
   width: auto;
-  background-color: #000000;
+  background-image: url(../src/assets/images/background.png);
+  background-size: cover;
+  background-position: center;
   padding: 2rem;
-  opacity: 65%;
   text-align: center;
-  text-overflow: clip;
   gap: 4rem;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.6);
+    z-index: 1;
+  }
+
+  & > * {
+    position: relative;
+    z-index: 2;
+  }
 `
 
 const Title = styled.p`
   font-size: 4rem;
   font-weight: bold;
   color: #FFFFFF;
+
+  /* Breakpoints para pantallas más pequeñas */
+  @media (max-width: 768px) {
+    font-size: 3rem; /* Pantallas medianas (tablets) */
+  }
+
+  @media (max-width: 480px) {
+    font-size: 2.5rem; /* Pantallas pequeñas (móviles) */
+  }
 `
 
 const Copy = styled.p`
   font-size: 1.5rem;
-  color: #666;
+  color: #DDDDDD;
   text-align: center;
   max-width: 800px;
   line-height: 1.6;
+
+  /* Breakpoints para pantallas más pequeñas */
+  @media (max-width: 768px) {
+    font-size: 1.25rem; /* Pantallas medianas (tablets) */
+    max-width: 90%; /* Ajustar el ancho en pantallas medianas */
+  }
+
+  @media (max-width: 480px) {
+    font-size: 1rem; /* Pantallas pequeñas (móviles) */
+    max-width: 95%; /* Ajustar el ancho en pantallas pequeñas */
+  }
 `
 
 const Welcome = () => {
     return (
-        <WelcomeContainer >
+        <WelcomeContainer>
             <Title>Tu tranquilidad,
                 <br></br>
                 Nuestra prioridad</Title>
